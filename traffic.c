b9d8daa3ce81dbe2fd7f6e0a73002212aab9d945
Dharsana Sakthi <dharsanasakthi.s2025cse@sece.ac.in>
2025-12-03 13:37:09 +0530

    Update traffic.c

--- traffic.c
+++ traffic.c
@@ -16,3 +16,38 @@ int main() {
         {"YELLOW", 2, "READY"},
         {"GREEN", 3, "GO"}
     };
+    printf("TRAFFIC SIGNAL SIMULATION\n");
+    printf("-----------------------------\n");
+    for (int i = 0; i < 3; i++) {
+        showSignal(&signals[i]);
+    }
+    return 0;
+}
+void showSignal(struct Signal *s) {
+    printf("\n=== %s LIGHT ===\n", s->name);
+    printf("Meaning: %s\n", s->meaning);
+
+    countdown(s->duration);
+    saveLog(s);
+}
+
+void countdown(int sec) {
+    for (int i = sec; i > 0; i--) {
+        printf("Time left: %d\n", i);
+        //fflush(stdout);
+        sleep(1);
+    }
+    printf("\n");
+}
+
+void saveLog(struct Signal *s) {
+    FILE *fp = fopen("signal_log.txt", "a");
+
+    if (fp == NULL) {
+        printf("Error opening file!\n");
+        return;
+    }
+    fprintf(fp, "%s | %d sec | %s \n",s->name, s->duration, s->meaning);
+
+    fclose(fp);
+}
